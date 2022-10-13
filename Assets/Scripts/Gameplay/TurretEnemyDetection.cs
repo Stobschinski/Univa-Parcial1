76dcd6cbd9d774d8b2777b18591154a76da3041a
Stobschinski <sandy.stobs@hotmail.com>
2022-10-13 08:01:30 -0500

    Clase Turrent

--- Assets/Scripts/Gameplay/TurretEnemyDetection.cs
+++ Assets/Scripts/Gameplay/TurretEnemyDetection.cs
@@ -7,18 +7,26 @@ public class TurretEnemyDetection : MonoBehaviour
     private GameObject _detectedEnemy = null;
     [SerializeField] private Transform _turrentPivot = null;
 
+    public Vector3 Vector3 { get; private set; }
+
     private void Update()
     {
         if (_detectedEnemy == null) return;
 
-        var direction:Vector3 = _detectedEnemy.transform.position - _turrentPivot.position;
+        var direction:Vector3 = _detectedEnemy.transform.position - transform.parent.position;
+        
         var targetRotation:Quaternion = Quaternion.LookRotation(forward: direction,
             upwards: Vector3.up);
         _turrentPivot.rotation = targetRotation;
+        var distance:float = Vector3.Distance(a: _detectedEnemy.transform.position, b: transform.parent.position);
 
+        if (Mathf.Abs(distance) > _maxDistance)
+        {
+            _detectedEnemy = null;
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Enemy") && _detectedEnemy == null)
         {
